d797fb01c8a682ecf794f3aec4efd43a2fe44ce2
royparreira <royparreira@gmail.com>
2022-06-25 18:36:04 -0300

    Initial Setup

---
+++ src/main/java/org/roy/trb/tst/credit/line/service/TrbTstCreditLineServiceApplication.java
@@ -0,0 +1,13 @@
+package org.roy.trb.tst.credit.line.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrbTstCreditLineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrbTstCreditLineServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/roy/trb/tst/credit/line/service/TrbTstCreditLineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roy.trb.tst.credit.line.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrbTstCreditLineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
